3431157795344525f7806edc8d8ca34004af2173
Dijana <deny.dijana@gmail.com>
2018-08-20 22:33:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ const form = document.querySelector('form');
 form.addEventListener('submit', formSubmited);
 const inputText = document.querySelector('#searchText');
 
-
-
-
 function formSubmited(e){
     const searchText = inputText.value;
     console.log(searchText);
     getMovies(searchText);
     e.preventDefault();
 }
-
-
-   
-
+// Get all movies
 function getMovies(searchText){
 
     //make request to api using axios
@@ -25,7 +19,7 @@ function getMovies(searchText){
         console.log(movies);
         let output = '';
             movies.forEach( movie => {
-          output+=`
+            output+=`
             <div class="col-md-3">
               <div class="card text-center p-3 mb-3">
                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
@@ -36,22 +30,22 @@ function getMovies(searchText){
             </div>
           `;
         });
-        
         document.querySelector('#movies').innerHTML = output;
       })
-      .catch(error => console.log(error));
-      
+      .catch(error => console.log(error)); 
   }
+
+// Get id to handle dinamic button event when full list is shown
   function movieSelected (id){
       sessionStorage.setItem('movieId', id);
       window.location = 'movie.html';
       return false;
   }
 
+// Get single movie
   function getSingleMovie(){
   let movieId = sessionStorage.getItem('movieId');
   axios.get("https://api.themoviedb.org/3/movie/"+ movieId + "?api_key=c9e28c9b1d0d87033d38b67bc5bfe1e3&append_to_response=images")
-  
     .then(response => {
       let movie = response.data;
       console.log(movie);
@@ -70,9 +64,7 @@ function getMovies(searchText){
           </div>
         </div>
       `;
-
       document.querySelector('#movie').innerHTML = output;
     })
     .catch(err => console.log(err));
-      
 }
